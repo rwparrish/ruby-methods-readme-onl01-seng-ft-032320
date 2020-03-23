2a88444b2fe06e12f62a162cd64f1c9022e1725f
Ryan Parrish <rino3717@gmail.com>
2020-03-23 18:40:20 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,16 +1,12 @@
 # Use this space to code along with the readme
-def say_hello_world_ten_times
-  phrase = "Hello World!"
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-end
 
-say_hello_world_ten_times
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
